5780a59a597a8f314f4178a682f7c814102d7c70
CharlesZuo01 <charlesqzuo@gmail.com>
2017-11-26 17:18:12 -0800

    Update Multicastmacconverter.py

--- Multicastmacconverter.py
+++ Multicastmacconverter.py
@@ -3,33 +3,52 @@ This script takes a multicast IP address and finds the multicast mac address.  T
 to binary string, then takes the hex value of every 4 chars in that string to find the mac
 '''
 
-line = '0000000100000000010111100' . #Beginning of mac address in binary format.
+print "There's no error checking, please be careful and enter IP addresses correctly"
+
+line = '0000000100000000010111100'
+
+first = [] #stores the first octet of multicast range, e.g 224,225, 226, etc... used to provide collision addresses
+second = 0 #second octet, will +/- 128 used to provide collision addresses
 
 ipinput = raw_input("Please enter your IP address: ")
-
 IP = ipinput.split('.')
-IP = [int(i) for i in IP[1:]] . #we don't care about the first octet because we only take last 23 bits of IP address to find mac
+firstoctet = IP[0]
+IP = [int(i) for i in IP[1:]]
+
+for i in range(224,240):
+	first.append(i)
+
+first.remove(int(firstoctet))
+
+if IP[0] <= 128:
+	second = IP[0] + 128
+else:
+	second = IP[0] - 128
 
 mcast = ''
 macaddr = ''
+#print IP
 
 for i in IP:
 	octet =  bin(i)[2:]
 	while (len(octet) < 8):
 		octet = '0' + octet
-	#print octet
 	mcast += octet
-	#print mcast
-	#print len(mcast)
 
 mcast = mcast[1:] #mcast is 24 bit number but mac address conversion only takes last 23 bits.
+
 line += mcast
 mac = [line[i:i+4] for i in range(0, len(line), 4)]
-
 for i in mac:
 	macaddr += hex(int(i, 2))[2:]
 
-print macaddr
+print 'Here is your MAC address:  ', macaddr
+print 'Here are the multicast addresses with the same mac address'
+for i in first:
+	print str(i) + '.' + str(second) + '.' + str(IP[1]) + '.' + str(IP[2])
+	print str(i) + '.' + str(IP[0]) + '.' + str(IP[1]) + '.' + str(IP[2])
+
+
 
 
 
